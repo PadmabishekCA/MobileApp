162c3dcace1a093e9a8063af3334c719675c882f
vthiravi <vthiravi@VTHIRAVI-XRFFC.partnet.cisco.com>
2016-06-17 15:29:19 +0530

    User story for ordering food

--- www/js/index.js
+++ www/js/index.js
@@ -11,43 +11,16 @@ var app = {
     getContactList: function(){
     	 console.log("Entering getContactList()");
     	    console.log("Exiting getContactList()");
+    	   
     }
+   
+    
     
 };
-$( "#target" ).click(function() {
-	  $.ajax({
-	      url : "http://www.w3schools.com/angular/customers_mysql.php",
-	      dataType:"json",
-	      cache: false,
-	      error:function (xhr, ajaxOptions, thrownError){
-	        debugger;
-	                alert(xhr.statusText);
-	                alert(thrownError);
-	            },
-	      success : function(json) {
-	        console.log("Entering getContactList.success()");
-	        console.log(JSON.stringify(json.records));
-	       
-	        	$.each(json.records, function(index,jsonObject){
-	        	    $.each(jsonObject, function(key,val){
-	        	    
-	        	    	
-	        	        console.log("key : "+key+" ; value : "+val);
-	        	        var html = '<li>' + key 
-	      	          + ' ' + val +'</li>';
-	      	          $('#contactList').append(html);
-	        	    	} );
-	        	});
-	         
-	        console.log("Exiting getContactList.success()");
-	        console.log(JSON.stringify(contactList));
-	      } 
-	    });
-	  console.log("End geeee");
-	});
 
+var uname;
 $( "#submitforcheck" ).click(function() {
-	var uname=$('#uname').val();
+	uname=$('#uname').val();
 	$.ajax({
 		url : "http://localhost:8080//confirmUser/"+uname,
 	      dataType:"json",
@@ -75,17 +48,17 @@ $( "#submitforcheck" ).click(function() {
 			        	    			            success : function(json) {
 			        	    			            	console.log(JSON.stringify(json.cuisine));
 			        	    			            	
+
 			        	    			            	$.each(json.cuisine, function(index,jsonObject){
 			        	    			            		var i=0;
 			        	    			            		var html='<li>';
 			        	    				        	    $.each(jsonObject, function(key,val){
 			        	    				        	    i=i+1;
-			        	    				        	    console.log("key : "+key+" ; value : "+val+ "index "+i);
-			        	    				        	    
-			        	    				        	    	
 			        	    				        	    if(key=='name')
 			        	    				        	    	{
-				        	    				        	   html +=val;
+			        	    				        	    	html+= '<a href="#" data-id="'+ val +'" >'+val+'</a>';
+				        	    				        	  
+				        	    				        	  
 			        	    				        	    	}
 			        	    				        	    else if(key=='description')
 			        	    				        	    	{
@@ -104,7 +77,11 @@ $( "#submitforcheck" ).click(function() {
 			        	    				        	    
 			        	    				        	    } );
 			        	    			            	});
-			        	    				        	
+			        	    			            	var obj = $('#foodList');
+			        	    			            	var str = $('#foodList').prop('outerHTML');
+			        	    			            	console.log("html str ---> "+str);
+			        	    			            	//console.log("html alertText ---> "+alertText);
+			        	    			            	console.log("html whole ---> "+obj);
 			        	    			            	$.each(json.games, function(index,jsonObject){
 			        	    			            		var i=0;
 			        	    			            		var html='<li>';
@@ -133,7 +110,7 @@ $( "#submitforcheck" ).click(function() {
 			        	    				        	    		}
 			        	    				        	    
 			        	    				        	    } );
-			        	    			            	});
+			        	    			            	} ) ;
 			        	    			            	
 			        	    			            	
 			        	    			            	 
@@ -174,8 +151,122 @@ $( "#fooditems" ).click(function() {
 		            
 	 });
 });
-$("#tab-1a").on(function(){
-	alert("heeello");
+
+
+
+$("#foodList").on("click", "a[data-id]", function() {
+	alert("hello");
+	
+    displayObject($(this).attr("data-id"));
+	//displayObject($(this).data("#id"));
 });
+//Order submission
+$("#order").click(function() {
+	$("#nameOfUserOrdering").val(uname);
+	var formData = $("form.orderForm").serializeArray();
+	console.log(JSON.stringify(formData));   
+	alert ("User who is ordering is "+ uname);
+	$.ajax({
+        type: "POST",
+        url: "http://localhost:8080/orderCuisine",
+        data: JSON.stringify({ classroom: formData }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+        	alert('Works!');
+        	console.log("Final here in js"+ JSON.stringify(response));
+        	var tHead = $('table#app-status-table thead');
+            
+            var headRow = "<tr><th data-priority='1'>Serial No</th>";
+            headRow += "<th data-priority='2'>Item Name</th>";
+            headRow += "<th data-priority='3'>Ordered Date </th>";
+            headRow += "<th data-priority='4'>Amount</th> </tr>";
+               
+            tHead.append(headRow);
+            console.log("html tHead ---> "+headRow);
+            var i=0;
+        	$.each(response.orderInfo, function(index,jsonObject){
+        		
+        		 var dateValue;
+         	    var amountValue;
+         	    var cusineValue;
+        		 i=i+1;
+        	    $.each(jsonObject, function(key,val){
+        	   
+        	   
+        	    console.log("key : "+key+" ; value : "+val+ "index "+i);
+        	    
+        	    	
+        	    if(key=='date')
+        	    	{
+        	    	dateValue = val;
+        	    	}
+        	    else if(key=='amount')
+        	    	{
+        	    	amountValue =val;
+        	    	}
+        	    if(key=='cuisineName')
+        	    	{
+        	    	cusineValue = val;
+        	    	}
+        	    	
+        	    
+        	    } );
+        	    var thedata={
+        		        serialNo: i,
+        		        cusineName: cusineValue,
+        		        date: dateValue,
+        		        amount: amountValue,
+        		        };
+        	    console.log("Inside the row binding" +JSON.stringify(thedata));
+        		    showAppStatusData(thedata);
+        	} ) ;
+        	
+        	
+        	console.log("Inside the logic done");
+        	$.mobile.changePage($('#application-status'));
+        	$("#app-status-table").table("refresh");
+        	
+            
+
+        	
+        	
+        },
+	    error      : function() {
+	        //console.log("error"+error);
+	        alert('Now working!');                  
+	    }
+	
+    });
+
+});
+
+
+
+function showAppStatusData(data) {
+    var pageData = data;
+   
+   
+    var tBody = $('#app-status-table tbody');
+    var theRow = "<tr><td>" + data.serialNo + "</td>";
+    theRow += "<td>" + data.cusineName + "</td>";
+    theRow += "<td>" + data.date + "</td>";
+    theRow += "<td>" + data.amount + "</td></tr>";
+    
+    tBody.append(theRow);
+   // var str = $('#foodList').prop('outerHTML');
+	console.log("html theRow ---> "+theRow);
+ //   $("#app-status-table").table("refresh");
+
+}
+function displayObject(testval) {
+    //var id = $(this).attr("data-id");
+    alert(testval);
+    
+    $.mobile.changePage($('#CuisineOrderPage'));
+    $('#fname').attr("value", testval);
+  
+};
+
 
 app.initialize();
